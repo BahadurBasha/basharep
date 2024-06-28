781469c5c0734bb313da4a38ea3bac36b1cd069e
Varshini <padmasrinivas.potlacherla@gmail.com>
2024-06-28 14:33:30 +0530

    uploading a new file

---
+++ sample.py
@@ -0,0 +1,9 @@
+def fact(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Example usage
+number=int(input("enter a number to find factorial : "))
+print(f"Factorial of {number} (iterative):", fact(number))
